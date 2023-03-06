799ebf76b8c044f5ed98f1d5cb134d3c55ac73d9
tunglv <luutungcntt6k61@gmail.com>
2023-03-06 13:20:01 +0700

    add icon

--- app/src/main/java/com/mobile/videocutter/MainActivity.kt
+++ app/src/main/java/com/mobile/videocutter/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.mobile.videocutter
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.mobile.videocutter.base.common.binding.BaseBindingActivity
+import com.mobile.videocutter.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseBindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
 }
