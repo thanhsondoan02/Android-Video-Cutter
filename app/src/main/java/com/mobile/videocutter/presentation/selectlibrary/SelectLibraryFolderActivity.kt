cd87b596255e919930d65b794ebde543c5dac044
linhdtn <hallinnh741@gmail.com>
2023-03-06 11:16:32 +0700

    UI library folder, edit convention color

---
+++ app/src/main/java/com/mobile/videocutter/presentation/selectlibrary/SelectLibraryFolderActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.videocutter.presentation.selectlibrary
+
+import com.mobile.videocutter.R
+import com.mobile.videocutter.base.common.binding.BaseBindingActivity
+import com.mobile.videocutter.databinding.SelectLibraryFolderActivityBinding
+
+class SelectLibraryFolderActivity : BaseBindingActivity<SelectLibraryFolderActivityBinding>(R.layout.select_library_folder_activity){
+    override fun onInitView() {
+        super.onInitView()
+
+    }
+}
