66d41008a5687a5068d2cab1b802e25ce4e13627
linhdtn <hallinnh741@gmail.com>
2023-03-07 17:40:51 +0700

    viewmodel library folder

--- app/src/main/java/com/mobile/videocutter/presentation/selectlibrary/SelectLibraryFolderViewModel.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/selectlibrary/SelectLibraryFolderViewModel.kt
@@ -1,4 +1,13 @@
 package com.mobile.videocutter.presentation.selectlibrary
 
+import com.mobile.videocutter.base.common.model.Album
+import com.mobile.videocutter.thread.FlowResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class SelectLibraryFolderViewModel {
+    private val _libraryFolderState = MutableStateFlow(FlowResult.newInstance<Album>())
+    val libraryFolderState = _libraryFolderState.asStateFlow()
+
+
 }
