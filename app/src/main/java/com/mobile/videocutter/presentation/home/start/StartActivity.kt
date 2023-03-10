d3f3f7454b594d77ac7a216548df044bd7b77e80
thomnt <thomnt@ftech.ai>
2023-03-10 10:27:35 +0700

    fix start activity th

--- app/src/main/java/com/mobile/videocutter/presentation/home/start/StartActivity.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/home/start/StartActivity.kt
@@ -1,17 +1,18 @@
-package com.mobile.videocutter.presentation.home
+package com.mobile.videocutter.presentation.home.start
 
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobile.videocutter.R
 import com.mobile.videocutter.base.common.binding.BaseBindingActivity
-import com.mobile.videocutter.databinding.HomeActivityBinding
+import com.mobile.videocutter.databinding.StartActivityBinding
 import com.mobile.videocutter.domain.model.mockLocalVideoList
 import com.mobile.videocutter.presentation.home.mystudio.MyStudioAdapter
 import com.mobile.videocutter.presentation.home.mystudio.MyStudioViewModel
+import com.mobile.videocutter.presentation.widget.recyclerview.LAYOUT_MANAGER_MODE
 
-class HomeActivity : BaseBindingActivity<HomeActivityBinding>(R.layout.home_activity) {
+class StartActivity : BaseBindingActivity<StartActivityBinding>(R.layout.start_activity) {
 
-    private val myStudioAdapter = MyStudioAdapter()
+    private val startAdapter = StartAdapter()
 
     private val viewModel by viewModels<MyStudioViewModel>()
 
@@ -21,10 +22,10 @@ class HomeActivity : BaseBindingActivity<HomeActivityBinding>(R.layout.home_acti
     }
 
     private fun initRecyclerView() {
-        binding.rvMyStudioVideoList.adapter = myStudioAdapter
-        binding.rvMyStudioVideoList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.crvMyStudioVideoList.setAdapter(startAdapter)
+        binding.crvMyStudioVideoList.setLayoutManagerMode(LAYOUT_MANAGER_MODE.LINEAR_HORIZATION)
 
-        myStudioAdapter.submitList(mockLocalVideoList(50).map { MyStudioAdapter.VideoDisplay(it) })
+        startAdapter.submitList(mockLocalVideoList(50).map { MyStudioAdapter.VideoDisplay(it) })
     }
 
 }
