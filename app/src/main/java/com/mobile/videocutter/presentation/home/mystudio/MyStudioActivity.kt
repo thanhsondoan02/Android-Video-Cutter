6fe14edf936b1bde90e2e5e4d8986a0a457396d2
thomnt <thomnt@ftech.ai>
2023-03-12 15:07:28 +0700

    fix

--- app/src/main/java/com/mobile/videocutter/presentation/home/mystudio/MyStudioActivity.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/home/mystudio/MyStudioActivity.kt
@@ -11,7 +11,6 @@ import com.mobile.videocutter.base.extension.setOnSafeClick
 import com.mobile.videocutter.base.extension.show
 import com.mobile.videocutter.databinding.MyStudioActivityBinding
 import com.mobile.videocutter.domain.model.LocalVideo
-import com.mobile.videocutter.presentation.adjust.LoadingFragment
 import com.mobile.videocutter.presentation.home.preview.PreviewVideoActivity
 import com.mobile.videocutter.presentation.model.IViewListener
 import handleUiState
