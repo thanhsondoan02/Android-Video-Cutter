1b94e687e0e4e3177264ccb0173d2abf9987df7a
thanhsondoan02 <thanhsondoan02@gmail.com>
2023-03-15 12:10:11 +0700

    temp

---
+++ app/src/main/java/com/mobile/videocutter/presentation/widget/video/TestActivity.kt
@@ -0,0 +1,9 @@
+package com.mobile.videocutter.presentation.widget.video
+
+import com.mobile.videocutter.R
+import com.mobile.videocutter.base.common.binding.BaseBindingActivity
+import com.mobile.videocutter.databinding.TestActivityBinding
+
+class TestActivity: BaseBindingActivity<TestActivityBinding>(R.layout.test_activity) {
+
+}
