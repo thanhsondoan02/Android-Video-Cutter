90ef49a314a6329e331a5b20267a16ae4b121df8
cuongvq <cuongvq@ftech.ai>
2023-03-15 16:41:38 +0700

    ui trim video

--- app/src/main/java/com/mobile/videocutter/presentation/home/start/StartActivity.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/home/start/StartActivity.kt
@@ -90,13 +90,15 @@ class StartActivity : BaseBindingActivity<StartActivityBinding>(R.layout.start_a
     private fun initRecyclerView() {
         startAdapter.listener = object : StartAdapter.IListener {
             override fun onVideoClick(localVideo: LocalVideo?) {
-                replaceFragment(
-                    PreviewVideoFragment(),
-                    bundleOf(
-                        PreviewVideoFragment.VIDEO_PATH to localVideo?.videoPath,
-                        PreviewVideoFragment.VIDEO_DURATION to localVideo?.duration
-                    )
-                )
+//                replaceFragment(
+//                    PreviewVideoFragment(),
+//                    bundleOf(
+//                        PreviewVideoFragment.VIDEO_PATH to localVideo?.videoPath,
+//                        PreviewVideoFragment.VIDEO_DURATION to localVideo?.duration
+//                    )
+//                )
+
+
             }
         }
         binding.crvStartVideoList.setAdapter(startAdapter)
