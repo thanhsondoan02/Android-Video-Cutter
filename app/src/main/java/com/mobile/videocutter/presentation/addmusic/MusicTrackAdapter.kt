3007c1a3e297f6398e6b2a1d81afcc3b353c5d32
thomnt <thomnt@ftech.ai>
2023-03-17 11:08:48 +0700

    add music 3

--- app/src/main/java/com/mobile/videocutter/presentation/addmusic/MusicTrackAdapter.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/addmusic/MusicTrackAdapter.kt
@@ -21,16 +21,20 @@ class MusicTrackAdapter : BaseAdapter() {
         return MusicTrackVH(binding as MusicTrackItemBinding)
     }
 
+    private fun unSelectOld() {
+        val indexOfOldSelect = dataList.indexOfFirst {
+            (it as? MusicTrackDisplay)?.isSelected == true
+        }
+        if(indexOfOldSelect!=-1){
+            (dataList[indexOfOldSelect] as? MusicTrackDisplay)?.isSelected = false
+            notifyItemChanged(indexOfOldSelect, SELECT_MUSIC_PAYLOAD)
+        }
+    }
+
     inner class MusicTrackVH(private val binding: MusicTrackItemBinding) : BaseVH<MusicTrackDisplay>(binding) {
         init {
             binding.root.setOnClickListener {
-                for (i in dataList.indices) {
-                    val musicTrackDisplay = dataList[i] as MusicTrackDisplay
-                    if (musicTrackDisplay.isSelected) {
-                        musicTrackDisplay.isSelected = false
-                        notifyItemChanged(i, SELECT_MUSIC_PAYLOAD)
-                    }
-                }
+                unSelectOld()
                 (dataList[adapterPosition] as? MusicTrackDisplay)?.let {
                     it.isSelected = true
                     notifyItemChanged(adapterPosition, SELECT_MUSIC_PAYLOAD)
@@ -62,16 +66,6 @@ class MusicTrackAdapter : BaseAdapter() {
         override fun onBind(data: MusicTrackDisplay, payloads: List<Any>) {
             if (payloads.contains(SELECT_MUSIC_PAYLOAD)) {
                 updateSelected(data.isSelected)
-            }
-        }
-
-        private fun unSelectOld() {
-            val indexOfOldSelect = dataList.indexOfFirst {
-                (it as? MusicTrackDisplay)?.isSelected == true
-            }
-            if(indexOfOldSelect!=-1){
-                (dataList[indexOfOldSelect] as? MusicTrackDisplay)?.isSelected = false
-                notifyItemChanged(indexOfOldSelect, SELECT_MUSIC_PAYLOAD)
             }
         }
 
