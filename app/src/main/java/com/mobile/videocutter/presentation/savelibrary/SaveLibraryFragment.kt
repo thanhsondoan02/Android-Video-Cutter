0f55500a88a5c7ecaeca6c2a48da3664cfb9a835
thanhsondoan02 <thanhsondoan02@gmail.com>
2023-03-19 23:56:32 +0700

    fix review

--- app/src/main/java/com/mobile/videocutter/presentation/savelibrary/SaveLibraryFragment.kt
+++ app/src/main/java/com/mobile/videocutter/presentation/savelibrary/SaveLibraryFragment.kt
@@ -1,5 +1,6 @@
 package com.mobile.videocutter.presentation.savelibrary
 
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.mobile.videocutter.R
@@ -8,22 +9,22 @@ import com.mobile.videocutter.base.extension.getAppString
 import com.mobile.videocutter.base.extension.setOnSafeClick
 import com.mobile.videocutter.databinding.SaveLibraryFragmentBinding
 import com.mobile.videocutter.presentation.model.IViewListener
+import com.mobile.videocutter.presentation.tasselsvideo.TasselsVideoViewModel
 import com.mobile.videocutter.presentation.widget.recyclerview.LAYOUT_MANAGER_MODE
 import handleUiState
+import loadImage
 
 class SaveLibraryFragment : BaseBindingFragment<SaveLibraryFragmentBinding>(R.layout.save_library_fragment) {
 
-    companion object {
-        const val LINKED = "https://www.youtube.com/"
-    }
-
     private val viewModel by viewModels<ShareViewModel>()
+    private val activityViewModel by activityViewModels<TasselsVideoViewModel>()
     private val shareInfoAdapter by lazy { ShareInfoAdapter() }
 
     override fun onInitView() {
         super.onInitView()
         initOnClick()
         setDataToListSocial()
+        binding.ivSaveLibrary.loadImage(activityViewModel.listPath?.firstOrNull())
     }
 
     fun initOnClick() {
@@ -32,6 +33,9 @@ class SaveLibraryFragment : BaseBindingFragment<SaveLibraryFragmentBinding>(R.la
         }
         binding.tvSaveLibrarySave.setOnSafeClick {
             baseActivity.showToast(getAppString(R.string.undeveloped_feature))
+        }
+        binding.hvSaveLibrary.setOnLeftIconClickListener {
+            backFragment()
         }
     }
 
